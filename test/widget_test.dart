557bca7b36af56b49751306835f283a6096d6a6e
Aniket Inamdar <asinamdar2024@gmail.com>
2025-02-02 01:06:50 +0530

    tempy frontend

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:lencho/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FarmApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
